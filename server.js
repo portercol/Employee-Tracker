62ab0ae7f237fdac845b314a0f52aa180309cd1b
Collin Porter <colls.potta@gmail.com>
2020-10-10 14:37:34 -0600

    added runSearch function with initial prompt of questions

--- server.js
+++ server.js
@@ -12,4 +12,39 @@ var con = mysql.createConnection({
 con.connect(function(err){
     if (err) throw err;
     console.log("Connected");
+    runSearch();
 });
+
+function runSearch() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "rawlist",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Employees By Department",
+          "View All Employees By Manager",
+          "Add Employee",
+        ]
+      })
+      .then(function(answer) {
+        switch (answer.action) {
+        case "View All Employees":
+        
+          break;
+  
+        case "View All Employees By Department":
+        
+          break;
+  
+        case "View All Employees By Manager":
+        
+          break;
+  
+        case "Add Employee":
+       
+          break;
+        }
+      });
+  };
