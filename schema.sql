ad7bb4985f43a13af955d7c7d585495c5b21cceb
Collin Porter <colls.potta@gmail.com>
2020-10-15 20:22:04 -0600

    added foreign keys to tables and added select statements

--- schema.sql
+++ schema.sql
@@ -3,27 +3,30 @@ CREATE DATABASE employee_tracker_DB;
 USE employee_tracker_DB;
 
 CREATE TABLE department (
-id INT NOT NULL AUTO_INCREMENT,
-department_name VARCHAR(30),
+id INT AUTO_INCREMENT NOT NULL,
+department_name VARCHAR(30) NOT NULL,
 PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-id INT NOT NULL AUTO_INCREMENT,
+id INT AUTO_INCREMENT NOT NULL,
 title VARCHAR(30) NOT NULL,
-salary DECIMAL(10,2) NOT NULL,
+salary DECIMAL NOT NULL default 0,
 department_id INT NOT NULL,
-PRIMARY KEY (id)
--- FOREIGN KEY (department_id) REFERENCES department (id)
+PRIMARY KEY (id),
+FOREIGN KEY (department_id) REFERENCES department (id)
 );
 
 CREATE TABLE employee (
-id INT NOT NULL AUTO_INCREMENT,
+id INT AUTO_INCREMENT NOT NULL,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
-role_id INT (10) NOT NULL,
-manager_id INT (10) NULL,
-PRIMARY KEY (id)
--- FOREIGN KEY (role_id) REFERENCES role (id),
--- FOREIGN KEY (manager_id) REFERENCES employee (id)
+role_id INT NOT NULL,
+manager_id INT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
